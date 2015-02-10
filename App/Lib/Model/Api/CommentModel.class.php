2f1e76b9a836d5f0ed5692bbdab6b151298c880a
zhuchencong <13472798110@163.com>
2015-02-09 16:52:22 -0800

    取消只能评论一次

--- App/Lib/Model/Api/CommentModel.class.php
+++ App/Lib/Model/Api/CommentModel.class.php
@@ -5,25 +5,19 @@ class CommentModel extends ApiBaseModel {
 
 	public function add_comment($id,$article_id,$comment_content)
 	{
-		$count = $this->where(array('article_id'=>$article_id,'user_id'=>$id))->count();
-		if($count==0)
-		{
-			$new_add = array(
-				'article_id' => $article_id,
-				'user_id' => $id,
-				'content' => $comment_content,
-				'create_time' => time()
-			);
+        $new_add = array(
+            'article_id' => $article_id,
+            'user_id' => $id,
+            'content' => $comment_content,
+            'create_time' => time()
+        );
 
-			$log = array('user_id'=>$id,'attention_id'=>$article_id,'status'=>1);
-			
-			parent::listen(__CLASS__,__FUNCTION__,$log);
+        $log = array('user_id'=>$id,'attention_id'=>$article_id,'status'=>1);
 
-			$bool = $this->add($new_add);
-			return $bool ? true : false;
-		}else{
-			return false;
-		}
+        parent::listen(__CLASS__,__FUNCTION__,$log);
+
+        $bool = $this->add($new_add);
+        return $bool ? true : false;
 	}
 
 	public function select_info($p,$index,$article_id)
