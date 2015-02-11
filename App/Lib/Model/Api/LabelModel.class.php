1f400d8b4e36f733c9e9dfc22d2a73e7d483bf45
zhuchencong <13472798110@163.com>
2015-02-10 21:51:41 -0800

    修改获得随机标签借口

--- App/Lib/Model/Api/LabelModel.class.php
+++ App/Lib/Model/Api/LabelModel.class.php
@@ -19,45 +19,49 @@
 				$where['label_name'] = array('like',$user_name.'%');
 				$label_list = $this->where($where)->getField('id',0);
 			}else{
-				$tag_list = $this->order('rand()')->limit(1)->field('id')->find();
-				$label_list[] = $tag_list['id'];
+                $label_list = $this->order('rand()')->limit(1)->getField('id',0);
 			}
 
-			$lab_li = D('LabelArticle')->where(array('label_id'=>array('IN',$label_list)))
-			->field('distinct article_id')->select();
+            if($label_list!='')
+            {
+                $lab_li = D('LabelArticle')->where(array('label_id'=>array('IN',$label_list)))
+                ->field('distinct article_id')->select();
 
-			$new_list = array();
+                $new_list = array();
 
-			foreach($lab_li as $value)
-			{
-				$new_list[] = $value['article_id'];
-			}
+                foreach($lab_li as $value)
+                {
+                    $new_list[] = $value['article_id'];
+                }
 
-			$arr_list = array();
+                $arr_list = array();
 
-			$list = D('Article')->where(array('id'=>array('IN',$new_list)))->limit($first,$offset)
-			->order('create_time desc')->select();
+                $list = D('Article')->where(array('id'=>array('IN',$new_list)))->limit($first,$offset)
+                ->order('create_time desc')->select();
 
-			$Users = D('Users');
+                $Users = D('Users');
 
-			$ContentPraise = D('ContentPraise');
+                $ContentPraise = D('ContentPraise');
 
-			foreach($list as $key=>$value)
-			{
-				$arr_list[$key]['user_info'] = $Users->where(array('u.id'=>$value['user_id']))->table('app_users as u')
-				->join('app_city as c on c.id = u.city_id and c.parent_id = 0')
-				->field('u.id,u.nickname,u.head_img,c.title')->find();
+                foreach($list as $key=>$value)
+                {
+                    $arr_list[$key]['user_info'] = $Users->where(array('u.id'=>$value['user_id']))
+                        ->table('app_users as u')->join('app_city as c on c.id = u.city_id')
+                        ->field('u.id,u.nickname,u.head_img,c.title')->find();
 
-				$arr_list[$key]['content'] = $value;
+                    $arr_list[$key]['content'] = $value;
 
-				parent::public_file_dir($arr_list[$key],array('head_img','article_img'));
+                    parent::public_file_dir($arr_list[$key],array('head_img','article_img'));
 
-				$arr_list[$key]['content']['time'] = date('Y-m-d H:i:s',$value['create_time']);
+                    $arr_list[$key]['content']['time'] = date('Y-m-d H:i:s',$value['create_time']);
 
-				$arr_list[$key]['content']['list_num'] = $ContentPraise->where(array('article_id'=>$value['id']))->count();
-			}
+                    $arr_list[$key]['content']['list_num'] = $ContentPraise->where(array('article_id'=>$value['id']))->count();
+                }
 
-			return $arr_list;
+			    return $arr_list;
+            }else{
+                return '';
+            }
 		}
 
         //上传标签
