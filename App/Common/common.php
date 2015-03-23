9700d0def7679d06d85dd8e455ff0e7429c94f53
zhuchencong <13472798110@163.com>
2015-03-23 01:15:59 -0700

    debug

--- App/Common/common.php
+++ App/Common/common.php
@@ -817,4 +817,14 @@ function pass_encryption ($str){
 
         return date('Ymd').'/'.$filename;
     }
+
+    function display_movie($url)
+    {
+        $path = pathinfo($url);
+        if($path['extension']!='mp4'){
+            return  '<img width="50" src="/'.C('PUBLIC_VISIT.app_dir').$url.'" />';
+        }else{
+            return  '<embed witdh="50" src="/'.C('PUBLIC_VISIT.app_dir').$url.'"><embed/>';
+        }
+    }
 ?>
